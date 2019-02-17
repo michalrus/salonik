72405dfc1e14c365dcaff2648a121fb807ae7552
Michal Rus <m@michalrus.com>
2019-02-17 16:53:07 +0100

    Save some µ-lecture (?)

---
+++ 1/Ex00.py
@@ -0,0 +1,5 @@
+a = 5 - 1
+a = a + 1
+
+b = f(x,y)
+g = lambda y: f(5,y)
---
+++ 1/Ex01.hs
@@ -0,0 +1,31 @@
+module Ex01 where
+
+f :: Maybe Integer
+f = Just 5 >>= (\x -> Just (x * 3)) >>= (\y -> Just (y - 3))
+
+f' :: Maybe Integer
+f' = do
+  x <- Just 5
+  y <- Just (x * 3)
+  Just (y - 3)
+
+yyy :: Maybe Integer -> Integer
+yyy (Just a) = a
+yyy Nothing = 42
+
+-----------------
+
+whosthere :: IO String
+whosthere = readFile "/etc/passwd"
+
+abomination :: IO String -> String
+abomination _ = "hai"
+
+xxx :: String -> [String]
+xxx s = lines s
+
+main :: IO ()
+main = do
+  wt :: String <- whosthere
+  print (xxx wt)
+  return wt
